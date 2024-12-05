7833062f681e2895bd84c5449f22a6164f5ce7a0
HyukE <ming2ki2@gmail.com>
2024-12-05 12:51:10 +0900

    feat: 메인 카드 스토리북 추가

---
+++ src/components/common/MainCard/MainCard.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+import MainCard from "./MainCard";
+import { MainCardProps } from "./MainCard.type";
+
+export default {
+  title: "Components/Common/MainCard",
+  component: MainCard,
+  args: {
+    backgroundColor: "#14213D",
+    CardLayout: "full",
+    children: "MainCard Test",
+    style: { padding: "10px", color: "white" },
+  },
+  argTypes: {
+    backgroundColor: { control: "color" },
+    CardLayout: {
+      control: { type: "select" },
+      options: ["full", "half"],
+    },
+    style: { control: "object" },
+    children: { control: "text" },
+  },
+} as Meta<MainCardProps>;
+
+const Template: StoryFn<MainCardProps> = (args) => <MainCard {...args} />;
+
+export const Default = Template.bind({});
